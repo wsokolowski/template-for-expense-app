c28277d79d5c58f044ef83bba15bfdc395fcfdfb
wsokolowski <w.sokolowski111@gmail.com>
2019-07-18 11:54:12 +0200

    One bug correctd

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
     require('dotenv').config({ path: '.env.test' });
-} else if (process_env.NODE_ENV === 'development') {
+} else if (process.env.NODE_ENV === 'development') {
     require('dotenv').config({ path: '.env.development' });
 }
 
