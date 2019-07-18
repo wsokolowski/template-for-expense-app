fd80452cd3329da5110415215fd894a71a3e83c7
wsokolowski <w.sokolowski111@gmail.com>
2019-07-18 14:27:57 +0200

    added fetching expenses from db

--- src/app.js
+++ src/app.js
@@ -3,22 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+import { startSetExpenses } from './actions/expenses';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 import './firebase/firebase';
 
 const store = configureStore();
-
-// store.dispatch(addExpense({ description: 'Water bill', amount: 4500, createdAt: 500 }));
-// store.dispatch(addExpense({ description: 'Gas bill', amount: 2000, createdAt: 1000 }));
-// store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: 200 }));
-
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
@@ -26,4 +16,10 @@ const jsx = (
     </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
+
+
--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -76,3 +76,12 @@ test('should not edit an expense if not found', () => {
     const state = expenseReducer(expenses, action);
     expect(state).toEqual(expenses);
 });
+
+test('should set expenses', () => {
+    const action = {
+        type: 'SET_EXPENSES',
+        expenses: [expenses[1]]
+    };
+    const state = expenseReducer(expenses, action);
+    expect(state).toEqual([expenses[1]]);
+});
